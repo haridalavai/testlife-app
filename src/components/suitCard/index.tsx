e33b9fd2e3475a386a9395eaaeb56ca717d67c7d
haridalavai <dalavai.hari@gmail.com>
2023-04-04 02:19:54 +0530

    fix build errors

--- src/components/suitCard/index.tsx
+++ src/components/suitCard/index.tsx
@@ -28,7 +28,6 @@ import {
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import DeleteDuiteModal from "../deleteSuiteModal";
-import Thumbnail from "../urlThumbnail";
 
 export interface ISuitCard {
   suite: any;
